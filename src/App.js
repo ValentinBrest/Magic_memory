8eecded30d13e89b793605bec2e0926f5d4c6f77
Valiantsin Kanapatski <79157063+ValentinBrest@users.noreply.github.com>
2022-03-13 11:18:16 +0300

    add function choice

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ const cardImages = [
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   // shuffle cards
   const shuffleCards = () => {
@@ -25,7 +27,11 @@ function App() {
     setTurns(0) 
   }
 
-  console.log(cards, turns)
+ // handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
@@ -33,7 +39,11 @@ function App() {
 
       <div className='card-grid'>
         {cards.map(card => (
-            <SingleCard card={card} key={card.id}/>
+            <SingleCard 
+              card={card} 
+              key={card.id}
+              handleChoice={handleChoice}
+            />
         ))}
 
       
